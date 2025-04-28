2e8aa36e06b283396493e17c47225844b8517a56
Lopez <Gerardo>
2025-04-28 15:11:13 -0500

    commit message

--- src/app/services/home/home.service.ts
+++ src/app/services/home/home.service.ts
@@ -12,29 +12,29 @@ export class HomeService {
     popular: MostPopularModel[] = [
         {
             id: 1,
-            name: 'Hotel 1',
-            location: 'Location 1',
+            name: 'Las Bahias',
+            location: 'Cineguilla, Lima',
             image: 'assets/hotels/popular/popular-1.svg',
             isPopular: true
         },
         {
             id: 2,
-            name: 'Hotel 2',
-            location: 'Location 2',
+            name: 'La isla',
+            location: 'San Bartolo, Lima',
             image: 'assets/hotels/popular/popular-2.svg',
             isPopular: true
         },
         {
             id: 3,
-            name: 'Hotel 3',
-            location: 'Location 3',
+            name: 'Laguna Azul',
+            location: 'Gineguilla, Lima',
             image: 'assets/hotels/popular/popular-3.svg',
             isPopular: false
         },
         {
             id: 4,
-            name: 'Hotel 4',
-            location: 'Location 4',
+            name: 'Garden Inn',
+            location: 'Asia, Lima',
             image: 'assets/hotels/popular/popular-4.svg',
             isPopular: false
         }
@@ -43,36 +43,36 @@ export class HomeService {
     offer: OfferModel[] = [
         {
             id: 1,
-            name: 'Hotel 1',
-            location: 'Location 1',
+            name: 'Sol y Mar',
+            location: 'Punta negra, Lima',
             image: 'assets/hotels/offers/offer-1.svg',
             offer: '20% off'
         },
         {
             id: 2,
-            name: 'Hotel 2',
-            location: 'Location 2',
+            name: 'Gran Hotel Imperial',
+            location: 'San Bartolo, Lima',
             image: 'assets/hotels/offers/offer-2.svg',
             offer: '30% off'
         },
         {
             id: 3,
-            name: 'Hotel 3',
-            location: 'Location 3',
+            name: 'Costa Dorada',
+            location: 'Punta Hermosa, Lima',
             image: 'assets/hotels/offers/offer-3.svg',
             offer: '50% off'
         },
         {
             id: 4,
-            name: 'Hotel 4',
-            location: 'Location 4',
+            name: 'Paraíso',
+            location: 'Punta Hermosa, Lima',
             image: 'assets/hotels/offers/offer-4.svg',
             offer: '50% off'
         },
         {
             id: 5,
-            name: 'Hotel 5',
-            location: 'Location 5',
+            name: 'Villa Serena',
+            location: 'Cineguilla, Lima',
             image: 'assets/hotels/offers/offer-5.svg',
             offer: '50% off'
         }
