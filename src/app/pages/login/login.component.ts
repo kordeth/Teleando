18a629866226401a913c75161a381ddc42693ece
Donovan Ramirez <donovanramirezc@hotmail.com>
2025-05-19 06:31:50 -0500

    Integrate login and registration with backend

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,22 +3,44 @@ import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '@services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  
   user = {
-    username: '',
-    password: ''
+    correo: '',
+    passwordUsuario: ''
   };
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
   onSubmit() {
-    console.log(this.user);
-  
+    this.authService.login(this.user).subscribe({
+      next: (res) => {
+        if (res.isSuccess) {
+          
+          this.authService.setSession(res.data);
+
+          alert('¡Inicio de sesión exitoso!');
+
+          this.router.navigate(['/']);
+        } else {
+          alert('Error: ' + res.errorMessage);
+        }
+      },
+      error: (err) => {
+        console.error(err);
+        alert('Error de conexión con el servidor');
+      }
+    });
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     baseUrl: 'https://acgjo5022j.execute-api.us-east-1.amazonaws.com/v1',
+    loginUrl: 'https://kganvp4pah.execute-api.us-east-1.amazonaws.com/S1',
     // baseUrl: 'http://localhost:3002/v1',
 };
